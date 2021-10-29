6f1640ca552361e0a3d74a916a4b5c46ea94d47e
Junichiro178 <bz.kitm.fj@gmail.com>
2021-10-29 23:50:04 +0900

    Ser Login UI
    ログイン画面を作成し、画面をタッチするとゲームが始まるようにしました

---
+++ CardGame/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{
+    // 画面をタッチしたらログインする
+    public void Login()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
